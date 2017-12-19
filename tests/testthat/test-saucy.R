59b1c92634f0e70d592fdd3669a15bfe7b2e85d9
boB Rudis <bob@rud.is>
2017-12-18 21:20:57 -0500

    C++11 switch and added basic tests

--- tests/testthat/test-saucy.R
+++ tests/testthat/test-saucy.R
@@ -1,6 +1,16 @@
 context("basic functionality")
 test_that("we can do something", {
 
-  #expect_that(some_function(), is_a("data.frame"))
+  x <- saucy::saucy(system.file("extdata", "graphfile", package="saucy"))
+  expect_is(x, "saucy")
+
+  y <- saucy::saucy(system.file("extdata", "graphfile2", package="saucy"))
+  expect_is(y, "saucy")
+
+  z <- saucy::shatter(system.file("extdata", "example.cnf", package="saucy"))
+  expect_is(z, "shatter")
 
 })
+
+
+
